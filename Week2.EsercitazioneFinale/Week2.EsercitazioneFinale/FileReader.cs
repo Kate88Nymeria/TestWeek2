be7182460a5f3b9ed0d90b2aec648dadd9aeb41b
Katia Caracciolo (c) <katia.caracciolo@avanade.com>
2021-09-17 15:51:37 +0200

    aggiornamento

--- Week2.EsercitazioneFinale/Week2.EsercitazioneFinale/FileReader.cs
+++ Week2.EsercitazioneFinale/Week2.EsercitazioneFinale/FileReader.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Week2.EsercitazioneFinale
 {
-    class FileReader
+    public class FileReader
     {
+        //public List<Good> ReadFile(string path)
+        //{
+        //    List<Good> goods = new List<Good>();
+        //    Good readedGood = null;
+
+        //    using (StreamReader reader = File.OpenText(path))
+        //    {
+        //        string line;
+        //        line = reader.ReadLine();
+
+        //        while ((line = reader.ReadLine()) != null)
+        //        {
+        //            string[] details = line.Split(';');
+        //            if (!details[5].Equals("-"))
+        //            {
+
+        //            }
+        //        }
+        //    }
+
+        //    return goods;
+        //}
     }
 }
